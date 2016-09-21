68da55d90abe9f6568685db88287aa5d0a6f6c7a
menglong.li <menglong.li@funplus.com>
2016-09-21 11:09:05 +0800

    Code format

--- code_snippets/asign_value_from_candidates.py
+++ code_snippets/asign_value_from_candidates.py
@@ -2,6 +2,6 @@
  Asign id with value from userid, and if userid is None, try snsid, fpid, user_id in order.
 '''
 id = next((item for item in [data_dict.get('userid', None),
-                               data_dict.get('snsid', None),
-                               data_dict.get('fpid', None),
-                               data_dict.get('user_id', None)] if item), None)
+                             data_dict.get('snsid', None),
+                             data_dict.get('fpid', None),
+                             data_dict.get('user_id', None)] if item), None)
