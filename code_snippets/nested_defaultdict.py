6425fc1e3e212b5625fd753b13ad677c413ac258
Menglong.Li <albert.menglongli@gmail.com>
2016-10-18 11:24:52 +0800

    Update nested_defaultdict.py

--- code_snippets/nested_defaultdict.py
+++ code_snippets/nested_defaultdict.py
@@ -1,18 +1,21 @@
+'''
+Usage:
+    r = tree()
+
+    r['a']['b']='bb'
+    r['a']['c']['d']='dddd'
+
+    import json
+    print json.dumps(r)
+    #{"a": {"c": {"d": "dddd"}, "b": "bb"}}
+
+    print r['a']['b']
+    #bb
+
+    print r['a']['c']['d']
+    #dddd
+'''
+
 from collections import defaultdict
 import json
-
 tree = lambda: defaultdict(tree)
-
-r = tree()
-
-r['a']['b']='bb'
-r['a']['c']['d']='dddd'
-
-print json.dumps(r)
-#{"a": {"c": {"d": "dddd"}, "b": "bb"}}
-
-print r['a']['b']
-#bb
-
-print r['a']['c']['d']
-#dddd
