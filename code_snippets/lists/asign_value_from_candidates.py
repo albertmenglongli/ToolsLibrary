49e7669d981c7542384228e67dafd89ab8ef90a7
Menglong.Li <albert.menglongli@gmail.com>
2017-03-27 12:58:06 +0800

    Update asign_value_from_candidates.py

--- code_snippets/lists/asign_value_from_candidates.py
+++ code_snippets/lists/asign_value_from_candidates.py
@@ -2,12 +2,15 @@
  Asign id with value from userid, and if userid is None, try snsid, fpid, user_id in order.
 '''
 # Method 1
+
 id = next((item for item in [data_dict.get('userid', None),
                              data_dict.get('snsid', None),
                              data_dict.get('fpid', None),
                              data_dict.get('user_id', None)] if item), None)
 
 # Method 2
+# Notice that this has Python recursive limitation
+
 def until(terminate, iterator, default):
     try:
         i = next(iterator)
@@ -21,3 +24,4 @@ def until(terminate, iterator, default):
 # candidates = [None, None, None, None]
 candidates = [None, None, '25', None]
 id = until(lambda x: x is not None, iter(candidates), '1')
+
