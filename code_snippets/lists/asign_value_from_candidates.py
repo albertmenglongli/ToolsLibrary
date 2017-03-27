676fa5f51e951b4bd54994f516d5cbb8096ec32b
Menglong.Li <albert.menglongli@gmail.com>
2017-03-27 11:57:01 +0800

    Update asign_value_from_candidates.py

--- code_snippets/lists/asign_value_from_candidates.py
+++ code_snippets/lists/asign_value_from_candidates.py
@@ -5,3 +5,18 @@ id = next((item for item in [data_dict.get('userid', None),
                              data_dict.get('snsid', None),
                              data_dict.get('fpid', None),
                              data_dict.get('user_id', None)] if item), None)
+
+
+def until(terminate, iterator, default):
+    try:
+        i = next(iterator)
+        if terminate(i):
+            return i
+        return until(terminate, iterator, default)
+    except StopIteration:
+        return default
+
+
+# candidates = [None, None, None, None]
+candidates = [None, None, '25', None]
+id = until(lambda x: x is not None, iter(candidates), '1')
