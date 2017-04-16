864b318af0ca3d593072ab80605e95f7421dc7d3
Menglong.Li <albert.menglongli@gmail.com>
2017-04-16 19:25:18 +0800

    refine the codes, and make it more common
    also with statement included.

--- code_snippets/coroutines/coroutine_pipeline.py
+++ code_snippets/coroutines/coroutine_pipeline.py
@@ -1,27 +1,6 @@
-'''
-# Usage: could be used as for-loop or responsibility-chain
-
-class Handler(object):
-
-    def __init__(self, msg_to_append):
-        self.msg_to_append = msg_to_append
-
-    def process(self, msg_lst):
-        msg_lst.append(self.msg_to_append)
-
-handlers = [Handler(' nice weather, right?'), Handler(' Good to hear from you.')]
-pipeline = gen_pipeline(*handlers)
-msg_lst = ['Got a letter']
-pipeline.send(msg_lst)
-print msg_lst  # ['Got a letter', ' nice weather, right?', ' Good to hear from you.']
-
-another_msg_lst = ['Reply']
-pipeline.send(another_msg_lst)  # ['Reply', ' nice weather, right?', ' Good to hear from you.']
-print another_msg_lst
-pipeline.close()
-'''
-
 import functools
+from contextlib import contextmanager
+
 
 def coroutine(function):
     """
@@ -38,14 +17,15 @@ def coroutine(function):
 
 
 @coroutine
-def gen_process_chain(handler, successor=None):
+def gen_process_chain(handler, handle_method_name, successor=None):
     try:
         while True:
             obj_to_handle = (yield)
             try:
-                handler.process(obj_to_handle)
+                getattr(handler, handle_method_name)(obj_to_handle)
             except Exception as e:
-                pass
+                import traceback
+                print(traceback.format_exc())
             if successor:
                 successor.send(obj_to_handle)
     except GeneratorExit:
@@ -53,12 +33,45 @@ def gen_process_chain(handler, successor=None):
             successor.close()
 
 
-def gen_pipeline(*args):
-    if not args:
+def gen_pipeline(handlers, handle_method_name):
+    if not handlers:
         raise Exception("should at least have one handler")
-    args_list = list(args)
-    args_list.reverse()
-    pipeline = gen_process_chain(args_list[0])
-    for method in args_list[1:]:
-        pipeline = gen_process_chain(method, pipeline)
-    return pipeline
+    handlers.reverse()
+    _pipeline_head_node = gen_process_chain(handlers[0], handle_method_name)
+    for handler in handlers[1:]:
+        _pipeline_head_node = gen_process_chain(handler, handle_method_name, _pipeline_head_node)
+    return _pipeline_head_node
+
+
+@contextmanager
+def responsibility_chain(*args):
+    _pipeline = None
+    try:
+        _pipeline = gen_pipeline(*args)
+        yield _pipeline
+    except Exception as e:
+        import traceback
+        print(traceback.format_exc())
+        raise e
+    finally:
+        if _pipeline:
+            _pipeline.close()
+
+
+if __name__ == '__main__':
+    class Handler(object):
+        def __init__(self, msg_to_append):
+            self.msg_to_append = msg_to_append
+
+        def process(self, msg_lst):
+            msg_lst.append(self.msg_to_append)
+
+
+    handlers = [Handler(' nice weather, right?'), Handler(' Good to hear from you.')]
+    with responsibility_chain(handlers, 'process') as chain:
+        msg_lst = ['Got a letter']
+        chain.send(msg_lst)
+        print(msg_lst)  # ['Got a letter', ' nice weather, right?', ' Good to hear from you.']
+        another_msg_lst = ['Reply']
+        chain.send(another_msg_lst)  # ['Reply', ' nice weather, right?', ' Good to hear from you.']
+        print(another_msg_lst)
