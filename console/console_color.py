e02c7d68f9ec05c06cbcedbede8b02935a02f622
menglongli <menglongli@micai.asia>
2017-07-26 11:37:56 +0800

    Add print colorful output in console.

---
+++ console/console_color.py
@@ -0,0 +1,30 @@
+class ConsoleColor:
+    # Color
+    BLACK = '\033[90m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    GRAY = '\033[97m'
+
+    # Style
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    # BackgroundColor
+    BgBLACK = '\033[40m'
+    BgRED = '\033[41m'
+    BgGREEN = '\033[42m'
+    BgORANGE = '\033[43m'
+    BgBLUE = '\033[44m'
+    BgPURPLE = '\033[45m'
+    BgCYAN = '\033[46m'
+    BgGRAY = '\033[47m'
+
+    # End
+    END = '\033[0m'
+
+
+print(ConsoleColor.BOLD + ConsoleColor.BgCYAN + ConsoleColor.WHITE + 'Hello World !' + ConsoleColor.END + ConsoleColor.END + ConsoleColor.END)
